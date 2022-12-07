4e9ce7036c816b9ebcc08567dc769499ecfc2ac5
Xardas <vadikradik@yandex.ru>
2022-12-07 16:41:07 +0300

    styles for task component

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -4,25 +4,30 @@ import "./task.css";
 const Task = ({task}) => {
     const {id, ...taskProps} = task;
   
-    let styleClass = "";
+    let taskClass = "task";
+    let viewClass = "task__view";
+    let descriptionClass = "task__description";
     let editingInput = null;
+
     if (taskProps.isCompleted) {
-      styleClass = "completed";
+      //taskClass = "completed";
+      descriptionClass = "task__description task__description--completed";
     } else if (taskProps.isEditing) {
-      styleClass = "editing";
-      editingInput = <input type="text" className="edit" value="Editing task" />;
+      taskClass = "task--editing";
+      editingInput = <input type="text" className="task__edit" value="Editing task" />;
+      viewClass = "task__view--editing";
     }
     
     return (
-      <li key={id} className={styleClass}>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>
-            <span className="description">{ taskProps.description }</span>
-            <span className="created">created 17 seconds ago</span>
+      <li key={id} className={taskClass}>
+        <div className={viewClass}>
+          <input className="task__toggle" type="checkbox" />
+          <label className="task__label">
+            <span className={descriptionClass}>{ taskProps.description }</span>
+            <span className="task__created-ago">created 17 seconds ago</span>
           </label>
-          <button className="icon icon-edit"></button>
-          <button className="icon icon-destroy"></button>
+          <button className="task__icon task__icon-edit"></button>
+          <button className="task__icon task__icon-destroy"></button>
         </div>
         { editingInput }
       </li>
